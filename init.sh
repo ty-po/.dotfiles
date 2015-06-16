8aadeaa870252a68ba7ff08331df138e52d57ba7
Tyler Polen <tyler.polen@gmail.com>
2015-06-16 05:15:30 +0000

    Changed git method

--- init.sh
+++ init.sh
@@ -4,7 +4,16 @@
 sudo apt-get update
 sudo apt-get install git vim tmux w3m vifm
 
-git init
-git remote add origin https://github.com/ty-po/.dotfiles.git
-git fetch --all
-git reset --hard origin/master
+git clone https://github.com/ty-po/.dotfiles.git
+
+cd .dotfiles
+
+cp -r .vim            ~/
+cp    .bash_aliases   ~/
+#cp    .bashrc.default ~/
+cp    .dircolors      ~/
+cp    .gitconfig      ~/
+cp    .gitignore      ~/
+cp    .tmux.conf      ~/
+cp    .vimrc          ~/
+
