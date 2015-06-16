28b7e7495b45c7c1dfdea5436bce322827f2132c
Tyler Polen <tyler.polen@gmail.com>
2015-06-16 05:00:46 +0000

    Git changes

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ sudo apt-get install git vim tmux w3m vifm
 
 git init
 git remote add origin https://github.com/ty-po/.dotfiles.git
-git fetch
-git checkout -t origin/setup
+git fetch --all
+git reset --hard origin/master
 
 sudo reboot
