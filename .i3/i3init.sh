fdfc698517a9e5e3c959217b0b89c0482a9b0994
Tyler Polen <tyler.polen@gmail.com>
2015-08-11 14:09:05 -0700

    Updated i3init to include ms core fonts

--- .i3/i3init.sh
+++ .i3/i3init.sh
@@ -5,7 +5,7 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo sh -c 'echo "deb http://ppa.launchpad.net/snwh/pulp/ubuntu/ trusty main" >> /etc/apt/sources.list.d/snwh-pulp.list'
 sudo apt-get update
-sudo apt-get install -y i3 suckless-tools lxappearance xfce4-terminal thunar redshift nitrogen paper-gtk-theme paper-icon-theme google-chrome-stable fonts-droid
+sudo apt-get install -y i3 suckless-tools lxappearance xfce4-terminal thunar redshift nitrogen paper-gtk-theme paper-icon-theme google-chrome-stable fonts-droid msttcorefonts
 
 cd ~
 
