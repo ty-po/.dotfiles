03a1ed3a348f37c3c38091fffce3d28c79143258
Tyler Polen <tyler.polen@gmail.com>
2019-09-13 15:50:16 -0400

    updated settings i think for nvidia, added compton

--- .i3/i3init.sh
+++ .i3/i3init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # i3 setup for debian derivatives
 
-sudo apt-get install -y i3 suckless-tools lxappearance vlc xfce4-terminal thunar redshift nitrogen google-chrome msttcorefonts xautolock python-pip unclutter dzen2
+sudo apt-get install -y i3 suckless-tools lxappearance vlc xfce4-terminal thunar redshift nitrogen google-chrome msttcorefonts xautolock python-pip unclutter dzen2 compton
 
 pip install --upgrade pip
 sudo pip install i3-py
@@ -12,3 +12,5 @@ rm -rf ~/.i3
 ln -sf    ~/.dotfiles/.i3             ~/
 
 ln -sf    ~/.dotfiles/.i3status.conf  ~/
+
+ln -sf    ~/.dotfiles/.i3/compton.conf  ~/.compton.conf
